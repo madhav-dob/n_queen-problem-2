2305aef5614f4d9150b5a65dacb31bd6d382f44d
madhavagarwal2404 <madhavagarwal2404@gmail.com>
2023-05-21 10:33:02 +0530

    nested loops method(method 1)

--- n_queen_1.c
+++ n_queen_1.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 int main () {
     int n;
-    n = 5;
-    printf("Age: ", n);
+    n = 8;
+    int temp = 1;
+    char matricks[n][n];
+    //matricks[0][0] = 0;
+// for (int i = 0;i < n; i ++){
+//     for (int j =0; j < n; j++){
+//         matricks[i][j] = '#';
+//     }
+// }
 
+//----------------------------------------------------
+//loop for setting elements to queen
+for (int i = 0;i < n; i ++){
+    if(temp < n){
+
+    }
+    else{
+        temp = 0;
+        }
+
+    for (int j = 0;j < n; j ++){
+        if ( j == temp) {
+                matricks[i][j] = 'o';
+        }
+        else{
+            matricks[i][j] = '#';
+        }
+    
+    
+    }
+temp += 2;
+
+}
+//loops end
+//-----------------------------------------------------
+//printing the list
+    int rowcount = n;
+    for (int i = 0;i < rowcount; i ++){
+        for (int j = 0;j < rowcount; j ++){
+    printf( "%c ",matricks[i][j]);
+    
+    }
+
+    printf("\n");
+
+    }
     return 0;
 }
+//printing end
